ee1fbc0b4075c06431b5f6125c4a87d1b81a5e6a
sh1ft <fedeshih@gmail.com>
2024-11-20 23:57:56 -0300

    feat: initial docker compose for all

--- service-registry/src/main/java/com/taskcomposer/service_registry/ServiceRegistryApplication.java
+++ service-registry/src/main/java/com/taskcomposer/service_registry/ServiceRegistryApplication.java
@@ -3,7 +3,13 @@ package com.taskcomposer.service_registry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+		DataSourceAutoConfiguration.class,
+		ConsulAutoConfiguration.class,
+		SecurityAutoConfiguration.class,
+		ManagementWebSecurityAutoConfiguration.class
+})
+@ComponentScan(basePackages = {"com.taskcomposer.service-registry.controllers"})
 public class ServiceRegistryApplication {
 
 	public static void main(String[] args) {
