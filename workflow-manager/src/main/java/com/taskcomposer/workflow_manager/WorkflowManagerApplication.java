8cf49bb06b086fc955354d68816a382d6f899610
frupnik <frupnik@itba.edu.ar>
2024-11-21 23:43:12 -0300

    fix: some changes went uncommited

--- workflow-manager/src/main/java/com/taskcomposer/workflow_manager/WorkflowManagerApplication.java
+++ workflow-manager/src/main/java/com/taskcomposer/workflow_manager/WorkflowManagerApplication.java
@@ -3,13 +3,12 @@ package com.taskcomposer.workflow_manager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.consul.ConsulAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(exclude = {
-		DataSourceAutoConfiguration.class,
 		ConsulAutoConfiguration.class,
 		SecurityAutoConfiguration.class,
 		ManagementWebSecurityAutoConfiguration.class
