64a6b67d624f9a5b860ae8d5effae21bcf15e3f7
frupnik <frupnik@itba.edu.ar>
2024-11-23 17:22:52 -0300

    fix: commented kafka code and fixed the docker composes

--- workflow-manager/src/main/java/com/taskcomposer/workflow_manager/services/ExecutionService.java
+++ workflow-manager/src/main/java/com/taskcomposer/workflow_manager/services/ExecutionService.java
@@ -8,23 +8,23 @@ import org.springframework.stereotype.Service;
 @Service
 public class ExecutionService {
 
-    private final KafkaTemplate<String, String> kafkaTemplate;
-
-    public ExecutionService(KafkaTemplate<String, String> kafkaTemplate) {
-        this.kafkaTemplate = kafkaTemplate;
-    }
-
-    public void executeWorkflow() {
-        try {
-            System.out.println(kafkaTemplate.send("executions", "hola").get());
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-    }
-
-    @KafkaListener(topics = "executions", groupId = "pepe")
-    public void listenGroupFoo(String message) {
-        System.out.println("Received Message in group pepe: " + message);
-    }
+//    private final KafkaTemplate<String, String> kafkaTemplate;
+//
+//    public ExecutionService(KafkaTemplate<String, String> kafkaTemplate) {
+//        this.kafkaTemplate = kafkaTemplate;
+//    }
+//
+//    public void executeWorkflow() {
+//        try {
+//            System.out.println(kafkaTemplate.send("executions", "hola").get());
+//        }catch (Exception e){
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    @KafkaListener(topics = "executions", groupId = "pepe")
+//    public void listenGroupFoo(String message) {
+//        System.out.println("Received Message in group pepe: " + message);
+//    }
 
 }
