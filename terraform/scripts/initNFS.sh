6a9119739c9de737ab6631ed529c483ad9b5c001
frupnik <frupnik@itba.edu.ar>
2025-02-07 18:38:44 -0300

    fix: nfs typo made the system read only >:(

--- terraform/scripts/initNFS.sh
+++ terraform/scripts/initNFS.sh
@@ -10,7 +10,7 @@ sudo chown -R nobody:nogroup /mnt/nfs
 sudo chmod 777 /mnt/nfs
 
 # Update the exports file
-echo "/mnt/nfs 172.31.0.0/16 (rw,sync,no_subtree_check,no_root_squash)" | sudo tee -a /etc/exports
+echo "/mnt/nfs 172.31.0.0/16(rw,sync,no_subtree_check,no_root_squash)" | sudo tee -a /etc/exports
 
 # Export the config and restart the NFS server
 sudo exportfs -a
