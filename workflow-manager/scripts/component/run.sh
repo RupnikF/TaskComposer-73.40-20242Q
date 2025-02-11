3d1005e0079ec431dfc8581ea91861e91ead6a3f
sh1ft <fedeshih@gmail.com>
2025-02-11 18:50:12 -0300

    feat: maven wrapper err

--- workflow-manager/scripts/component/run.sh
+++ workflow-manager/scripts/component/run.sh
@@ -3,6 +3,6 @@
 # Run
 cd ../../
 
-./mvnw dependency:go-offline -B
+mvn dependency:go-offline -B
 
-./mvnw test -Dgroup=component -DKAFKA_HOST=localhost -DKAFKA_TOPIC=test -DPOSTGRES_HOST=postgres -DPOSTGRES_PORT=5432 -DPOSTGRES_DB=workflow -DPOSTGRES_USER=workflow -DPOSTGRES_PASSWORD=workflow
+mvn test -Dgroup=component -DKAFKA_HOST=localhost -DKAFKA_TOPIC=test -DPOSTGRES_HOST=postgres -DPOSTGRES_PORT=5432 -DPOSTGRES_DB=workflow -DPOSTGRES_USER=workflow -DPOSTGRES_PASSWORD=workflow
--- workflow-manager/scripts/unit/run.sh
+++ workflow-manager/scripts/unit/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd ../../
 
-./mvnw dependency:go-offline -B
-./mvnw test -Dgroup=unit -DKAFKA_HOST=localhost -DKAFKA_TOPIC=test -DPOSTGRES_HOST=postgres -DPOSTGRES_PORT=5432 -DPOSTGRES_DB=workflow -DPOSTGRES_USER=workflow -DPOSTGRES_PASSWORD=workflow
+mvn dependency:go-offline -B
+mvn test -Dgroup=unit -DKAFKA_HOST=localhost -DKAFKA_TOPIC=test -DPOSTGRES_HOST=postgres -DPOSTGRES_PORT=5432 -DPOSTGRES_DB=workflow -DPOSTGRES_USER=workflow -DPOSTGRES_PASSWORD=workflow
