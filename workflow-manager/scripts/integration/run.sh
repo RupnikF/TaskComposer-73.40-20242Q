361e61cd9cd13fee8c755525b887ac37c383fc5f
sh1ft <fedeshih@gmail.com>
2025-02-11 18:44:05 -0300

    fix: use same image as Dockerfile for java

--- workflow-manager/scripts/integration/run.sh
+++ workflow-manager/scripts/integration/run.sh
@@ -3,4 +3,5 @@
 # Run
 cd ../../
 
+./mvnw dependency:go-offline -B
 ./mvnw test -Dgroup=integration -DKAFKA_HOST=localhost -DKAFKA_TOPIC=test -DPOSTGRES_HOST=postgres -DPOSTGRES_PORT=5432 -DPOSTGRES_DB=workflow -DPOSTGRES_USER=workflow -DPOSTGRES_PASSWORD=workflow
--- workflow-manager/scripts/unit/run.sh
+++ workflow-manager/scripts/unit/run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 cd ../../
-mvn test -Dgroup=unit -DKAFKA_HOST=localhost -DKAFKA_TOPIC=test -DPOSTGRES_HOST=postgres -DPOSTGRES_PORT=5432 -DPOSTGRES_DB=workflow -DPOSTGRES_USER=workflow -DPOSTGRES_PASSWORD=workflow
+
+./mvnw dependency:go-offline -B
+./mvnw test -Dgroup=unit -DKAFKA_HOST=localhost -DKAFKA_TOPIC=test -DPOSTGRES_HOST=postgres -DPOSTGRES_PORT=5432 -DPOSTGRES_DB=workflow -DPOSTGRES_USER=workflow -DPOSTGRES_PASSWORD=workflow
