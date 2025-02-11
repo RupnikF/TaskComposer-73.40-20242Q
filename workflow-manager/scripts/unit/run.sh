e3771768b284f917d26074f37f2cb8ad9a88691e
sh1ft <fedeshih@gmail.com>
2025-02-11 18:08:45 -0300

    chore: trying to fix test in workflow manager

--- workflow-manager/scripts/unit/run.sh
+++ workflow-manager/scripts/unit/run.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 cd ../../
-
 mvn test -Dgroup=unit -DKAFKA_HOST=localhost -DKAFKA_TOPIC=test -DPOSTGRES_HOST=postgres -DPOSTGRES_PORT=5432 -DPOSTGRES_DB=workflow -DPOSTGRES_USER=workflow -DPOSTGRES_PASSWORD=workflow
